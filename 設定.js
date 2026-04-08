64a0da70ddd245afa046b5c0425d82e3dc452fb7
t-h1015 <t-hiramatsu.1015@gmail.com>
2026-04-08 10:03:09 +0900

    共通localkeyの作成

--- 設定.js
+++ 設定.js
@@ -5,6 +5,6 @@ const CONFIG = {
   LINE_TOKEN: props.getProperty('LINE_TOKEN'),
   LIFF_ID: props.getProperty('LIFF_ID'),
   MASTER_SHEET_NAME: props.getProperty('MASTER_SHEET_NAME') || "依頼一覧",
-  SUPPLY_SHEET_NAME: props.getProperty('SUPPLY_SHEET_NAME') || "備品発注一覧",
+  SUPPLY_SHEET_NAME: props.getProperty('SUPPLY_SHEET_NAME') || "備品注文",
   TARGET_USER_ID: props.getProperty('TARGET_USER_ID')
 };
